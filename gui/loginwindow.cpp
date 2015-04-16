8799f78671f19861ee1cf60523fc7a8da2df1e1c
Nicola Dalla Costa <dalla.costa.nicola@gmail.com>
2015-04-16 15:45:37 +0200

    Changes in direcory hierarchy. Added gui directory.

---
+++ gui/loginwindow.cpp
@@ -0,0 +1,6 @@
+#include "loginwindow.h"
+
+LoginWindow::LoginWindow(QWidget *parent) :
+    QDialog(parent)
+{
+}
---
+++ gui/loginwindow.h
@@ -0,0 +1,18 @@
+#ifndef LOGINWINDOW_H
+#define LOGINWINDOW_H
+
+#include <QDialog>
+
+class LoginWindow : public QDialog {
+    Q_OBJECT
+private:
+
+public:
+    explicit LoginWindow(QWidget *parent = 0);
+signals:
+
+public slots:
+
+};
+
+#endif // LOGINWINDOW_H
