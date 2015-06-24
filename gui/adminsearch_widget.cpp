18090bde67a10cb6389961e8b799cf47e4df606e
Nicola Dalla Costa <dalla.costa.nicola@gmail.com>
2015-06-24 17:34:53 +0200

    Aggiunta la ricerca per tipo nella versione per l'amministratore. Aggiunto un pulsante per resettare la ricerca. Migliorato lo stile.

--- gui/adminsearch_widget.cpp
+++ gui/adminsearch_widget.cpp
@@ -2,7 +2,10 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QPushButton>
+#include <QCheckBox>
+#include <QDebug>
 
+// COSTRUTTORE AdminSearchWidget
 AdminSearchWidget::AdminSearchWidget( QWidget *parent ) :
     QWidget( parent )
 {
@@ -12,14 +15,43 @@ AdminSearchWidget::AdminSearchWidget( QWidget *parent ) :
 
     search = new QLineEdit();
 
-    QPushButton *searchButton = new QPushButton( tr( "Search" ) );
+    checkbox = new QWidget;
+
+    QVBoxLayout *checkboxLayout = new QVBoxLayout;
+
+    checkboxEntry1 = new QCheckBox( "Basic" );
+    checkboxEntry2 = new QCheckBox( "Express" );
+    checkboxEntry3 = new QCheckBox( "Business" );
+
+    checkboxLayout->addWidget( checkboxEntry1 );
+    checkboxLayout->addWidget( checkboxEntry2 );
+    checkboxLayout->addWidget( checkboxEntry3 );
+
+    checkbox->setLayout( checkboxLayout );
+
+    searchButton = new QPushButton( tr( "Search" ) );
+    connect( searchButton, SIGNAL( clicked() ), this, SLOT( searchUsers() ) );
+    searchButton->setStyleSheet(
+        "QPushButton { border: 2px solid #003D5C; border-radius: 5px;"
+            "background: #003D5C; color: white; }"
+        "QPushButton:pressed { border: 2px solid #00527A; background: #00527A; }"
+    );
+
+    resetButton = new QPushButton( tr( "Reset" ) );
+    resetButton->setStyleSheet(
+        "QPushButton { border: 2px solid #FF1744; border-radius: 5px;"
+            "background: #FF1744; color: white; }"
+        "QPushButton:pressed { border: 2px solid #FF5252; background: #FF5252; }"
+    );
 
     QWidget *bottomFiller = new QWidget();
     bottomFiller->setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Expanding );
 
     layout->addWidget( searchLabel );
     layout->addWidget( search );
+    layout->addWidget( checkbox );
     layout->addWidget( searchButton );
+    layout->addWidget( resetButton );
     layout->addWidget( bottomFiller );
 
     setLayout( layout );
@@ -27,8 +59,11 @@ AdminSearchWidget::AdminSearchWidget( QWidget *parent ) :
     setStyleSheet(
         "QLabel { color: white; }"
         "QLineEdit { border: 1px solid gray; border-radius: 5px; background: white; }"
-        "QPushButton { border: 2px solid #003D5C; border-radius: 5px;"
-            "background: #003D5C; color: white; }"
-        "QPushButton:pressed { border: 2px solid #00527A; background: #00527A; }"
+        "QCheckBox { color: white; }"
     );
+}
+
+// SLOT AdminSearchWidget::searchUsers()
+void AdminSearchWidget::searchUsers() {
+
 }
--- gui/adminsearch_widget.h
+++ gui/adminsearch_widget.h
@@ -3,20 +3,29 @@
 
 #include <QWidget>
 #include <QLineEdit>
+#include <QPushButton>
 #include <QCheckBox>
 
 class AdminSearchWidget : public QWidget {
     Q_OBJECT
 private:
     QLineEdit *search;
-
-    QCheckBox *userType;
+    QWidget *checkbox;
+    QCheckBox *checkboxEntry1;
+    QCheckBox *checkboxEntry2;
+    QCheckBox *checkboxEntry3;
+    QPushButton *searchButton;
+    QPushButton *resetButton;
 public:
+    /** Costruttore esplicito ad 1 parametro con 1 parametro di default.
+     *
+     * @param QWidget  Puntatore al QWidget padre. Se nullo si riferisce a quello top-level.
+     */
     explicit AdminSearchWidget( QWidget *parent = 0 );
 signals:
 
 public slots:
-
+    void searchUsers();
 };
 
 #endif // SEARCH_WIDGET_H
