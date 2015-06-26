4d74041311ccc98db06e7513c8a8ebdc909e0123
Nicola Dalla Costa <dalla.costa.nicola@gmail.com>
2015-06-26 12:10:36 +0200

    Opzioni avanzate di ricerca per l'amministratore a comparsa/scomparsa.

--- gui/adminsearch_widget.cpp
+++ gui/adminsearch_widget.cpp
@@ -1,6 +1,5 @@
 #include "gui/adminsearch_widget.h"
 #include <QVBoxLayout>
-#include <QLabel>
 #include <QPushButton>
 #include <QCheckBox>
 #include <QDebug>
@@ -9,33 +8,58 @@
 AdminSearchWidget::AdminSearchWidget( QWidget *parent ) :
     QWidget( parent )
 {
-    QVBoxLayout *layout = new QVBoxLayout();
+    layout = new QVBoxLayout( this );
 
-    QLabel *searchLabel = new QLabel( tr( "Search user" ) );
+    searchLabel = new QLabel( tr( "Search user" ) );
 
-    search = new QLineEdit();
+    search = new QLineEdit( this );
 
-    checkbox = new QWidget;
+    searchWidget = new QWidget( this );
 
-    QVBoxLayout *checkboxLayout = new QVBoxLayout;
+    QHBoxLayout *searchWidgetLayout = new QHBoxLayout( searchWidget );
+    searchWidgetLayout->setMargin( 0 );
 
-    checkboxEntry1 = new QCheckBox( "Basic" );
-    checkboxEntry2 = new QCheckBox( "Express" );
-    checkboxEntry3 = new QCheckBox( "Business" );
+    searchButton = new QPushButton( tr( "Search" ), searchWidget );
+    searchButton->setStyleSheet(
+        "QPushButton { border: 2px solid #003D5C; border-radius: 5px;"
+            "background: #003D5C; color: white; }"
+        "QPushButton:pressed { border: 2px solid #00527A; background: #00527A; }"
+    );
+    connect( searchButton, SIGNAL( clicked() ), this, SLOT( searchUsers() ) );
+
+    searchSettingsButton = new QPushButton( searchWidget );
+    searchSettingsButton->setIcon( QIcon( QPixmap( ":/icons/icon/settings.png" ) ) );
+    searchSettingsButton->setMaximumWidth( 24 );
+    searchSettingsButton->setStyleSheet( "border: none" );
+    connect( searchSettingsButton, SIGNAL( clicked() ), this, SLOT( showAdvancedSettings() ) );
+
+    hideSearchSettingsButton = new QPushButton( searchWidget );
+    hideSearchSettingsButton->setIcon( QIcon( QPixmap( ":/icons/icon/chevron-up.png" ) ) );
+    hideSearchSettingsButton->setMaximumWidth( 24 );
+    hideSearchSettingsButton->setStyleSheet( "border: none" );
+    hideSearchSettingsButton->setVisible( false );
+    connect( hideSearchSettingsButton, SIGNAL( clicked() ), this, SLOT( hideAdvancedSettings() ) );
+
+    checkbox = new QWidget( this );
+
+    QVBoxLayout *checkboxLayout = new QVBoxLayout( checkbox );
+
+    checkboxEntry1 = new QCheckBox( "Basic", checkbox );
+    checkboxEntry2 = new QCheckBox( "Express", checkbox );
+    checkboxEntry3 = new QCheckBox( "Business", checkbox );
 
     checkboxLayout->addWidget( checkboxEntry1 );
     checkboxLayout->addWidget( checkboxEntry2 );
     checkboxLayout->addWidget( checkboxEntry3 );
 
     checkbox->setLayout( checkboxLayout );
+    checkbox->setVisible( false );
 
-    searchButton = new QPushButton( tr( "Search" ) );
-    connect( searchButton, SIGNAL( clicked() ), this, SLOT( searchUsers() ) );
-    searchButton->setStyleSheet(
-        "QPushButton { border: 2px solid #003D5C; border-radius: 5px;"
-            "background: #003D5C; color: white; }"
-        "QPushButton:pressed { border: 2px solid #00527A; background: #00527A; }"
-    );
+    searchWidgetLayout->addWidget( searchButton );
+    searchWidgetLayout->addWidget( searchSettingsButton );
+    searchWidgetLayout->addWidget( hideSearchSettingsButton );
+
+    searchWidget->setLayout( searchWidgetLayout );
 
     resetButton = new QPushButton( tr( "Reset" ) );
     resetButton->setStyleSheet(
@@ -49,8 +73,8 @@ AdminSearchWidget::AdminSearchWidget( QWidget *parent ) :
 
     layout->addWidget( searchLabel );
     layout->addWidget( search );
+    layout->addWidget( searchWidget );
     layout->addWidget( checkbox );
-    layout->addWidget( searchButton );
     layout->addWidget( resetButton );
     layout->addWidget( bottomFiller );
 
@@ -61,6 +85,20 @@ AdminSearchWidget::AdminSearchWidget( QWidget *parent ) :
         "QLineEdit { border: 1px solid gray; border-radius: 5px; background: white; }"
         "QCheckBox { color: white; }"
     );
+}
+
+// SLOT AdminSearchWidget::showAdvancedSettings()
+void AdminSearchWidget::showAdvancedSettings() {
+    searchSettingsButton->setVisible( false );
+    hideSearchSettingsButton->setVisible( true );
+    checkbox->setVisible( true );
+}
+
+// SLOT AdminSearchWidget::hideAdvancedSettings()
+void AdminSearchWidget::hideAdvancedSettings() {
+    searchSettingsButton->setVisible( true );
+    hideSearchSettingsButton->setVisible( false );
+    checkbox->setVisible( false );
 }
 
 // SLOT AdminSearchWidget::searchUsers()
--- gui/adminsearch_widget.h
+++ gui/adminsearch_widget.h
@@ -5,16 +5,24 @@
 #include <QLineEdit>
 #include <QPushButton>
 #include <QCheckBox>
+#include <QLabel>
+#include <QVBoxLayout>
 
 class AdminSearchWidget : public QWidget {
     Q_OBJECT
 private:
+    QVBoxLayout *layout;
+    QLabel *searchLabel;
     QLineEdit *search;
     QWidget *checkbox;
     QCheckBox *checkboxEntry1;
     QCheckBox *checkboxEntry2;
     QCheckBox *checkboxEntry3;
+    QWidget *searchWidget;
+    QVBoxLayout *searchWidgetLayout;
     QPushButton *searchButton;
+    QPushButton *searchSettingsButton;
+    QPushButton *hideSearchSettingsButton;
     QPushButton *resetButton;
 public:
     /** Costruttore esplicito ad 1 parametro con 1 parametro di default.
@@ -25,6 +33,10 @@ public:
 signals:
 
 public slots:
+    void showAdvancedSettings();
+
+    void hideAdvancedSettings();
+
     void searchUsers();
 };
 
