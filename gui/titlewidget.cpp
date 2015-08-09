28b9486406732cf897b33e786ff0ad5fdaf6989c
Nicola Dalla Costa <dalla.costa.nicola@gmail.com>
2015-08-09 17:12:18 +0200

    [Client] Migliorata UI TitleWidget.

--- gui/titlewidget.cpp
+++ gui/titlewidget.cpp
@@ -10,16 +10,29 @@ TitleWidget::TitleWidget( const SmartTitolo& st, QWidget *parent ) :
     QVBoxLayout *layout = new QVBoxLayout( this );
 
     schoolLabel = new QLabel( st->getSchool() );
-    dateAttendedLabel = new QLabel( st->getDateAttended().toString( "yyyy/MM/dd" ) );
-    degreeLabel = new QLabel( st->getDegree() );
+    schoolLabel->setStyleSheet( "QLabel { color: rgba( 0, 0, 0, 0.87 ); font: bold; }" );
     fieldOfStudyLabel = new QLabel( st->getFieldOfStudy() );
+    fieldOfStudyLabel->setStyleSheet( "QLabel { color: rgba( 0, 0, 0, 0.87 ); }" );
+    dateAttendedLabel = new QLabel( st->getDateAttended().toString( "yyyy/MM/dd" ) );
+    dateAttendedLabel->setStyleSheet( "QLabel { color: rgba( 0, 0, 0, 0.54 ); }" );
+    degreeLabel = new QLabel( st->getDegree() );
     gradeLabel = new QLabel( st->getGrade() );
 
+    QWidget *moreInfoWidget = new QWidget( this );
+
+    QHBoxLayout *moreInfoLayout = new QHBoxLayout( moreInfoWidget );
+    moreInfoLayout->setMargin( 0 );
+
+    QWidget *moreInfoFiller = new QWidget( moreInfoWidget );
+    moreInfoFiller->setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Expanding );
+
+    moreInfoLayout->addWidget( fieldOfStudyLabel );
+    moreInfoLayout->addSpacing( 30 );
+    moreInfoLayout->addWidget( dateAttendedLabel );
+    moreInfoLayout->addWidget( moreInfoFiller );
+
     layout->addWidget( schoolLabel );
-    layout->addWidget( dateAttendedLabel );
-    layout->addWidget( degreeLabel );
-    layout->addWidget( fieldOfStudyLabel );
-    layout->addWidget( gradeLabel );
+    layout->addWidget( moreInfoWidget );
 
     setLayout( layout );
 }
